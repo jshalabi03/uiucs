ec0acf01b0fd74c2d498f1190cca1d5619ee4c34
Jacob Shalabi <jshalabi03@gmail.com>
2023-11-16 13:28:23 -0600

    made repsonsive

--- src/components/course-card.tsx
+++ src/components/course-card.tsx
@@ -8,23 +8,32 @@ interface CourseCardParams {
 
 export default function CourseCard({ course }: CourseCardParams) {
   return (
-    <Link href={`/courses/${course.subjectCode}${course.courseNumber}`} passHref>
+    <Link
+      href={`/courses/${course.subjectCode}${course.courseNumber}`}
+      passHref
+    >
       <div className="cursor-pointer">
         <Card className="transform transition duration-300 ease-in-out hover:scale-105 hover:bg-orange-200 border border-gray-300">
-          <div className="flex justify-between items-center bg-amber-100 p-4">
+          <div className="flex flex-col space-y-4 md:space-y-0 md:flex-row md:justify-between items-center bg-orange-200 p-4">
             <div>
-              <div className="font-bold text-lg text-black">
+              <div className="font-bold text-xl md:text-2xl text-black text-center md:text-left">
                 {course.subjectCode} {course.courseNumber}
               </div>
-              <div className="text-gray-700">{course.title}</div>
+              <div className="text-gray-700 text-left md:text-center">
+                {course.title}
+              </div>
             </div>
             <div className="grid grid-cols-3 gap-4">
               <div className="text-center">
-                <div className="font-bold text-orange-500">{course.difficulty}</div>
+                <div className="font-bold text-orange-500">
+                  {course.difficulty}
+                </div>
                 <div className="text-xs text-gray-600">Difficulty</div>
               </div>
               <div className="text-center">
-                <div className="font-bold text-orange-500">{course.reviews}</div>
+                <div className="font-bold text-orange-500">
+                  {course.reviews}
+                </div>
                 <div className="text-xs text-gray-600">Reviews</div>
               </div>
               <div className="text-center">
@@ -34,8 +43,10 @@ export default function CourseCard({ course }: CourseCardParams) {
             </div>
           </div>
           <div className="p-4 bg-white">
-            <div className="flex justify-between items-center">
-              <div className="text-gray-700">Credit: {course.creditHoursStr}</div>
+            <div className="flex flex-col md:flex-row md:justify-between items-center">
+              <div className="text-gray-700">
+                Credit: {course.creditHoursStr}
+              </div>
               <div className="flex items-center">
                 <div className="text-gray-700 mr-2">Rating:</div>
                 <div className="font-bold text-orange-500 text-lg">
