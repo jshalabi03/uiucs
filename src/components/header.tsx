7fa60ffa00a80903c2fcb08d9e6ebb7d118804b2
Ritul Soni <rsoni27@illinois.edu>
2023-11-18 13:06:08 -0600

    added logo to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-
 import UserNav from "./user-nav";
 import { Button } from "./ui/button";
 import React from "react";
@@ -10,18 +9,28 @@ import { signIn, useSession } from "next-auth/react";
 export default function Header() {
   const { data } = useSession();
 
+  const logo = (
+    <img 
+      src="/Logo.png" 
+      alt="Logo" 
+      className="absolute top-1/2 left-1 -translate-y-1/2 h-16" // Tailwind classes for positioning and size
+    />
+  );
+
   if (data?.user) {
     return (
-      <div className="border border-b border-blue-950 bg-orange-300 flex items-center justify-between p-4">
-        <Link href="/">Home</Link>
+      <div className="relative border border-b border-blue-950 bg-orange-300 flex items-center justify-between px-4 py-2">
+        {logo}
+        <Link href="/" className="pl-24">Home</Link>
         <UserNav user={data.user} />
       </div>
     );
   }
 
   return (
-    <div className="border border-b border-blue-950 bg-orange-300 flex items-center justify-between p-4">
-      <Link href="/">Home</Link>
+    <div className="relative border border-b border-blue-950 bg-orange-300 flex items-center justify-between px-4 py-2">
+      {logo}
+      <Link href="/" className="pl-24">Home</Link>
       <Button
         variant="outline"
         onClick={() => {
